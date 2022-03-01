6c6f6aab7099c04bf6bc6e5567c71eeea5ff956b
Davide-22 <daviderenzetti59@gmail.com>
2022-03-01 13:06:43 +0100

    Signup ts and minor fixes

--- Front-End/src/app/sign-up/sign-up.component.ts
+++ Front-End/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,13 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  formError : string; 
+
+  constructor(public fb: FormBuilder) { 
+    this.form = fb.group({
+      'user' : ['',Validators.required],
+      'email' : ['',Validators.required],
+      'password' : ['',Validators.required],
+      'repeatpassword' : ['',Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
 
-  submit(): void {
-    window.location.href="";
+  checkPassword() : boolean{
+    if(this.form.controls['password'].value.length < 8){
+      this.formError = "Password's length must be at least 8 characters";
+      return true;
+    }
+    return false;
   }
-
+  send() : void{
+    if(!this.form.valid){
+      this.formError = "You must fill all the fields";
+      return;
+    }
+    if(this.checkPassword()){
+      return;
+    }
+    window.location.href="";
+    console.log(this.form.controls['user'].value);
+  }
 }
+
