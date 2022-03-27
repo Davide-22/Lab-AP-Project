d49f86dc2c56b30614a3fa4ed8eb48c8a1e49931
Stefano <servillostefano@gmail.com>
2022-03-27 18:28:29 +0200

    bar chart compare page

--- Front-End/src/app/compare-page/compare-page.component.ts
+++ Front-End/src/app/compare-page/compare-page.component.ts
@@ -1,9 +1,12 @@
 import { CompileMetadataResolver } from '@angular/compiler';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import {compares} from '../compares';
 import { Compare } from '../models/compare.model';
 import { ExpenseService } from '../services/expense.service';
 import { Travel } from '../travels';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-compare-page',
@@ -14,6 +17,9 @@ export class ComparePageComponent implements OnInit {
 
   @Input() public travels: Travel[];
   public expenses: Compare[];
+  public compares: string[] = ['accomodation','food','transport','cultural place','event'];
+  public travel1: string="";
+  public travel2: string="";
 
   constructor(private readonly expenseService: ExpenseService) { }
 
@@ -26,4 +32,61 @@ export class ComparePageComponent implements OnInit {
     })
   }
 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    scales: { x: {}, y: { min: 0}},
+    plugins: {
+      legend: { display: true,},
+      datalabels: { anchor: 'end', align: 'end'}
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [
+    DataLabelsPlugin
+  ];
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: []
+  };
+
+  chartInitialization(): void{
+    this.barChartData.labels = this.compares;
+    var travels = [{data: [], label:this.travel1},{data: [], label:this.travel2}]
+    this.barChartData.datasets = travels;
+
+    for(let i=0;i<this.compares.length;i++){
+      var test1: boolean = false;
+      var test2: boolean = false;
+      for(let j=0;j<this.expenses.length;j++){
+        if(this.expenses[j].category==this.compares[i]){
+          if(this.expenses[j].name == this.travel1){
+            test1 = true;
+            this.barChartData.datasets[0].data.push(this.expenses[j].avg);
+          }else if(this.expenses[j].name == this.travel2){
+            test2 = true;
+            this.barChartData.datasets[1].data.push(this.expenses[j].avg);
+          }
+        }
+      }
+      if(test1 == false){
+        this.barChartData.datasets[0].data.push(0);
+      }
+      if(test2 == false){
+        this.barChartData.datasets[1].data.push(0);
+      }
+      this.chart?.update();
+    }
+  }
+
+  selectTravel(travel:string,id:number): void {
+    if(id == 1){
+      this.travel1 = travel;
+    }else {
+      this.travel2 = travel;
+    }
+    this.chartInitialization();
+  }
 }
